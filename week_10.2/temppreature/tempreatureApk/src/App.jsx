724d48f6b8d48605cebc49d048b4469fef14db73
Anmol Bansiwal <anmolbansiwal1@gmail.com>
2025-11-05 08:12:35 +0530

    tempreatureAPK initialized

---
+++ week_10.2/temppreature/tempreatureApk/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import { tempreatureAtom } from './store/atoms/tempreatureAtom.js'
+import './App.css'
+
+function App() {
+  
+}
+
+export default App
---
+++ week_10.2/temppreature/tempreatureApk/src/store/atoms/tempreatureAtom.js.js
@@ -0,0 +1,5 @@
+import { atom } from 'recoil';
+export const tempreatureAtom= atom({
+    key: 'tempreatureAtom',
+    value:25,  /// default tempreaure atom
+});
