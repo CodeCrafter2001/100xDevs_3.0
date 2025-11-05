903674fd549dbdd5df30829beec537f4ef307c6c
Anmol Bansiwal <anmolbansiwal1@gmail.com>
2025-11-05 15:12:04 +0530

    done with the components

--- week_10.2/temppreature/tempreatureApk/src/App.jsx
+++ week_10.2/temppreature/tempreatureApk/src/App.jsx
@@ -1,9 +1,44 @@
 import { useState } from 'react'
+import { RecoilRoot } from 'recoil'
 import { tempreatureAtom } from './store/atoms/tempreatureAtom.js'
 import './App.css'
+import { farenthiteSelector } from './store/selectors/farenthiteSelector.js'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 
-function App() {
-  
+//components for displaying tempreature and controling tempreature
+function displayTempreature(){
+  const celsius = useRecoilValue(tempreatureAtom);
+  const fahrenheit = useRecoilValue(farenthiteSelector);
+  return(
+    <>
+    <div>
+      <h2>Tempreate App using recoil</h2>
+    <p>celsius : {celsius} °C</p>
+    <p>Fahrenheit: {fahrenheit.toFixed(2)} °F</p>
+    </div>
+    </>
+  ); 
 }
+function tempreatureController(){
+  const setTemp= useSetRecoilState(tempreatureAtom);
+  return(
+    <div>
+      <button onClick={()=> setTemp(t => t+1)}>Increase Tempreature °C </button>
+      <button onClick={()=> setTemp(t => t-1)}>Decrease tempreature °C</button>
+    </div>
+  );
+}
+function App() {
+  return(
+    <>
+    <RecoilRoot>
+      <displayTempreature/>
+      <tempreatureController/>
+    </RecoilRoot>
+    </>
+  );
+
+}
+
 
 export default App
---
+++ week_10.2/temppreature/tempreatureApk/src/store/selectors/farenthiteSelector.js
@@ -0,0 +1,10 @@
+import { tempreatureAtom } from "../atoms/tempreatureAtom.js";
+import { selector } from "recoil";
+
+export const farenthiteSelector= selector({
+    key: 'farenthiteSelector',
+    get: ({get})=>{
+        const celsius= get(tempreatureAtom);
+        return (celsius * 9)/5 +32;
+    },
+});
