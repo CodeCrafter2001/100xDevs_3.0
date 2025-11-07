d450be67f5159a53fdeadfd709a8b3161e38979b
Anmol Bansiwal <anmolbansiwal1@gmail.com>
2025-11-07 23:04:52 +0530

    random

---
+++ week 11/week-11.3RecoilDeepDive/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ return (
+  <>
+  <div className="div_button">
+  <button>My netwok</button>
+  <button>Jobs</button>
+  <button>Messaging</button>
+  <button>Notification</button>
+  </div>
+  </>
+ );  
+}
+
+export default App
