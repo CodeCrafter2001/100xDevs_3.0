a45fcffc53b958cad986ef9008c169e3157bd44d
Anmol Bansiwal <anmolbansiwal1@gmail.com>
2025-11-12 08:22:12 +0530

    learning selectors

--- week 11/week-11.3RecoilDeepDive/src/App.jsx
+++ week 11/week-11.3RecoilDeepDive/src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useMemo, useState } from 'react';
+import './App.css';
+import { networkAtom, jobsAtom, notificationAtom, messagingAtom , totalNotificationSelector} from './atoms';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 
 function App() {
- return (
-  <>
-  <div className="div_button">
-  <button>My netwok</button>
-  <button>Jobs</button>
-  <button>Messaging</button>
-  <button>Notification</button>
-  </div>
-  </>
- );  
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  );
 }
 
-export default App
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
+  const notificationAtomCount = useRecoilValue(notificationAtom);
+  const totalNotificationCount= useRecoilValue(totalNotificationSelector);
+  return (
+    <div className="div_button">
+      <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs ({jobsAtomCount})</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notification ({notificationAtomCount})</button>
+      <button> Me ({totalNotificationCount})</button>
+    </div>
+  );
+}
+
+export default App;
